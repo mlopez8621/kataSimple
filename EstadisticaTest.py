07dba0f67613c886852849262eb935c17a278e50
yeramirezs <ye.ramirez26@uniandes.edu.co>
2018-09-29 19:45:54 -0500

    Iteracion 1 ciclo 2: Enviar un numero (Rojo)

--- EstadisticaTest.py
+++ EstadisticaTest.py
@@ -6,3 +6,5 @@ class EstadisticaTest(TestCase):
     def test_cantElementos(self):
         self.assertEqual(Estadistica().cantElementos(""),[0],"Cadena vacia")
 
+    def test_cantElementosUnNumero(self):
+        self.assertEqual(Estadistica().cantElementos("1"),[1],"Un Numero")
