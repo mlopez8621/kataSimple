e14dd5543576f29ba4e070750266b9a00e136224
yeramirezs <ye.ramirez26@uniandes.edu.co>
2018-09-29 21:07:54 -0500

    Iteraciòn 3 ciclo 4: Correcciòn Devolver un vector con la cantidad de elementos, el menor y el mayor(Verde)

--- Estadistica.py
+++ Estadistica.py
@@ -27,4 +27,14 @@ class Estadistica:
         elif len(cadena) == 1:
             return [1,1,1]
         else:
-            return [2,1,2]
+            cadenaStr = cadena.split(",")
+            menor = cadenaStr[0]
+            mayor = cadenaStr[0]
+            for ele in cadenaStr:
+                if ele < menor:
+                    menor = ele
+            for ele in cadenaStr:
+                if ele > mayor:
+                    mayor = ele
+            cantEle = len(cadenaStr)
+            return [cantEle, int(menor), int(mayor)]
