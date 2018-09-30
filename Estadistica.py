afb399142aefefc0292fd1ea965f193e686a9d2f
mlopeza1 <mlopeza1@ucentral.edu.co>
2018-09-29 21:02:39 -0500

    Iteraciòn 3 ciclo 3: Correcciòn devolver un vector con la cantidad de elementos, el menor y el mayor (Verde)

--- Estadistica.py
+++ Estadistica.py
@@ -24,5 +24,7 @@ class Estadistica:
     def mayor(self,cadena):
         if cadena == "":
             return [0,0,0]
-        else:
+        elif len(cadena) == 1:
             return [1,1,1]
+        else:
+            return [2,1,2]
